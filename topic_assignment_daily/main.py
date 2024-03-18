7fc7ecad051df25b32d84bcf863a50d05b17a337
VoeP <volterpaukku@gmail.com>
2024-03-18 11:42:14 +0100

    adding new cloud funciton

---
+++ topic_assignment_daily/main.py
@@ -0,0 +1,42 @@
+import functions_framework
+from reddit_sentiment_modules.scraping_python_functions import *
+from reddit_sentiment_modules.pub_sub_messaging import *
+from datetime import date
+
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    cred = Credentials()    
+    reddit = init_reddit()
+
+    subreddits = ["wallstreetbets", "worldnews", "askreddit"]
+    #print(reddit)
+    df_l = get_comments_from_hot(reddit, subreddit=subreddits[0], num = 10)
+    print(f"{subreddits[0]} done")
+    df_l["subreddit"]=subreddits[0]
+    for subreddit in subreddits[1:]:
+        df_r = get_comments_from_hot(reddit, subreddit=subreddit, num = 10)
+        df_r["subreddit"]=subreddit
+        df_l = pd.concat([df_l, df_r])
+        print(f"{subreddit} done")
+    df=df_l
+    df["date"] = str(date.today())
+    df.to_gbq(destination_table=f"reddit_comment_data.raw_daily_hot_comments",
+          project_id='ml-deployments-practice',
+          if_exists="append")
+
+    success_message = f'Moving data to GBQ successful on the {date.today()}'
+
+    publish_success_message(success_message)
+    return success_message
---
+++ topic_assignment_daily/topic_api/topic_api.py
@@ -0,0 +1,60 @@
+
+from google.cloud import storage, language_v1, bigquery, language_v2
+import pandas as pd
+
+
+def assign_topic(date=None):
+    # Set up our GCS, NL, and BigQuery clients
+    storage_client = storage.Client()
+    nl_client = language_v1.LanguageServiceClient()
+
+
+    # TODO: replace YOUR_PROJECT with your project id below
+    bq_client = bigquery.Client(project='ml-deployments-practice')
+
+    dataset_ref = bq_client.dataset('reddit_comment_data')
+    dataset = bigquery.Dataset(dataset_ref)
+    table_ref = dataset.table('posts_by_date_and_sub') # Update this if you used a different table name
+    table = bq_client.get_table(table_ref)
+
+# Send article text to the NL API's classifyText method
+    def classify_text(article):
+        response_topic = nl_client.classify_text(
+                document=language_v1.types.Document(
+                        content=article,
+                        type_='PLAIN_TEXT'
+                )
+        )
+
+        return response_topic
+    rows_for_bq = []  
+    client = bigquery.Client()
+    df = client.list_rows(table).to_dataframe()
+    if date:
+        df = df[df["date"] == date]
+    #df['text'] = df['text'].str.replace(',', ' ').str.replace('\n', ' ')
+    print(df)
+    # Send files to the NL API and save the result to send to BigQuery
+    i=0
+    for ind, data in df.iterrows():
+        print(i/df.shape[0])
+        i=i+1
+        try:
+            comment_text = bytes(data['text'], 'utf-8')
+            nl_topic = classify_text(comment_text)
+                #if len(nl_topic.categories) > 0:
+            if 1==1:
+                rows_for_bq.append((str(comment_text), 
+                            str(nl_topic.categories.name), nl_topic.categories.confidence))
+        except Exception as e:
+            #print(e)
+            pass
+
+    print(rows_for_bq)
+    print("Writing NL API article data to BigQuery...")
+    # Write article text + category data to BQ
+
+    output_table_ref = dataset.table('post_topics')
+    output_table = bq_client.get_table(output_table_ref)
+    errors = bq_client.insert_rows(output_table, rows_for_bq)
+    assert errors == []
